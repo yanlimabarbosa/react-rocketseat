af6cea4ac80d09e6cdea3f40cb72cb7ed74dc53d
Yan Lima Barbosa <yanbr763@gmail.com>
2024-08-17 22:54:48 -0300

    CSS Modules

--- 01-fundamentals-reactjs/src/App.jsx
+++ 01-fundamentals-reactjs/src/App.jsx
@@ -1,8 +1,11 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
+import "./styles.css"
 
 export function App() {
   return (
     <>
+      <Header />
       <Post
         author="Yan Barbosa"
         content="Lorem ipsum dolor sit ametmos laborum officiis laboriosam culpa totam quisquam, mollitia exercitationem nostrum aut voluptatibus!"
---
+++ 01-fundamentals-reactjs/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import { header } from "./Header.module.css"
+
+export function Header() {
+  return (
+    <header className={header}>
+      <strong>Ignite Feed</strong>
+    </header>
+  )
+}
