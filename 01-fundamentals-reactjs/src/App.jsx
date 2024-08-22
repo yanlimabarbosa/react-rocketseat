c3b9be28d9d9e32d2aab4222ed4b541a9bb82571
Yan Lima Barbosa <yanbr763@gmail.com>
2024-08-21 23:50:32 -0300

    Component: Sidebar

--- 01-fundamentals-reactjs/src/App.jsx
+++ 01-fundamentals-reactjs/src/App.jsx
@@ -1,16 +1,25 @@
 import { Header } from "./components/Header"
 import { Post } from "./Post"
+
+import styles from "./App.module.css"
+
 import "./global.css"
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
     <>
       <Header />
-      <Post
-        author="Yan Barbosa"
-        content="Lorem ipsum dolor sit ametmos laborum officiis laboriosam culpa totam quisquam, mollitia exercitationem nostrum aut voluptatibus!"
-      />
-      <Post author="Joseph Benter" content="Really cool!" />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Yan Barbosa"
+            content="Lorem ipsum dolor sit ametmos laborum officiis laboriosam culpa totam quisquam, mollitia exercitationem nostrum aut voluptatibus!"
+          />
+          <Post author="Joseph Benter" content="Really cool!" />s
+        </main>
+      </div>
     </>
   )
 }
