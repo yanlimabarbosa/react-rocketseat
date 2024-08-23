2d7ec0d69ed6f5bf7fcd5a520980b40753ca2f5d
Yan Lima Barbosa <yanbr763@gmail.com>
2024-08-22 22:16:57 -0300

    Component: Post

---
+++ 01-fundamentals-reactjs/src/components/Post.jsx
@@ -0,0 +1,35 @@
+import styles from "./Post.module.css"
+
+export function Post({ author, content }) {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://avatars.githubusercontent.com/u/129100238?v=4"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Yan Barbosa</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title="11 de maio às 08:13h" dateTime="2025-05-11 08:13:30">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋 </p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀{" "}
+        </p>
+        <p>👉 <a href="">yan.repo/doctorcare</a></p>
+        <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+
+        
+      </div>
+    </article>
+  )
+}
