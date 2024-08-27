ac974be09e17279cc99ed4473aede971f87a2404
Yan Lima Barbosa <yanbr763@gmail.com>
2024-08-26 23:35:44 -0300

    Post Properties: add date, time and information to posts
    Adds date and time information to posts, including a relative timestamp and a formatted date/time string. The time is now displayed as a relative timestamp, e.g., "Published 2 hours ago", and the full date and time are available on hover. This enhances the user experience by providing context to the post's publication date.

--- 01-fundamentals-reactjs/src/components/Post.jsx
+++ 01-fundamentals-reactjs/src/components/Post.jsx
@@ -1,26 +1,63 @@
+import { format, formatDistanceToNow } from "date-fns"
+import ptBR from "date-fns/locale/pt-BR"
+
 import { Avatar } from "./Avatar"
 import { Comment } from "./Comment"
+
 import styles from "./Post.module.css"
 
-export function Post({ author, content }) {
+export function Post({ author, content, publishedAt }) {
+  // const publishedDateFormatted = new Intl.DateTimeFormat("pt-BR", {
+  //   day: "2-digit",
+  //   month: "long",
+  //   hour: "2-digit",
+  //   minute: "2-digit",
+  //   // timeStyle: "full",
+  // }).format(publishedAt)
+
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    { locale: ptBR }
+  )
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://avatars.githubusercontent.com/u/129100238?v=4" />
+          <Avatar src={author?.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Yan Barbosa</strong>
-            <span>Web Developer</span>
+            <strong>{author?.name}</strong>
+            <span>{author?.role}</span>
           </div>
         </div>
 
-        <time title="11 de maio às 08:13h" dateTime="2025-05-11 08:13:30">
-          Publicado há 1h
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋 </p>
+        {content.map((line) => {
+          if (line.type === "paragraph") {
+            return <p>{line.content}</p>
+          } else if (line.type === "link") {
+            return (
+              <p>
+                <a href="">{line.content}</a>
+              </p>
+            )
+          }
+        })}
+        {/* <p>Fala galeraa 👋 </p>
         <p>
           Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
           no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀{" "}
@@ -32,7 +69,7 @@ export function Post({ author, content }) {
           <a href="">#novoprojeto</a> {""}
           <a href="">#nlw</a> {""}
           <a href="">#rocketseat</a>
-        </p>
+        </p> */}
       </div>
 
       <form className={styles.commentForm}>
