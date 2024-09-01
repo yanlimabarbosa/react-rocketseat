e492a7e56e92b6b84d0bc56cda1665aedffe2101
Yan Lima Barbosa <yanbr763@gmail.com>
2024-09-01 20:46:34 -0300

    Comment Likes

--- 01-fundamentals-reactjs/src/components/Comment.jsx
+++ 01-fundamentals-reactjs/src/components/Comment.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react"
 import { Avatar } from "./Avatar"
 import styles from "./Comment.module.css"
 import { ThumbsUp, Trash } from "phosphor-react"
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0)
+
   function handleDeleteComment() {
     onDeleteComment(content)
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1)
   }
 
   return (
@@ -32,9 +39,9 @@ export function Comment({ content, onDeleteComment }) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
