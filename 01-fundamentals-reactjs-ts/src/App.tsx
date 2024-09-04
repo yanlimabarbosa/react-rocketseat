fd2f4b2d130186904a49026a3b69ff5d0e83ac67
Yan Lima Barbosa <yanbr763@gmail.com>
2024-09-03 22:54:29 -0300

    Typescript fixes

--- 01-fundamentals-reactjs-ts/src/App.tsx
+++ 01-fundamentals-reactjs-ts/src/App.tsx
@@ -1,12 +1,13 @@
 import { Header } from "./components/Header"
 import { Sidebar } from "./components/Sidebar"
 import { Post } from "./components/Post"
+import { PostType } from "./components/Post"
 
 import styles from "./App.module.css"
 
 import "./global.css"
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -53,14 +54,7 @@ export function App() {
         <Sidebar />
         <main>
           {posts.map((post) => {
-            return (
-              <Post
-                key={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
-              />
-            )
+            return <Post key={post.id} post={post} />
           })}
         </main>
       </div>
