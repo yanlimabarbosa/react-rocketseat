e7b51f2a4eacd1a5e8ae5d0b7ccd2e9536d0df6d
Yan Lima Barbosa <yanbr763@gmail.com>
2024-09-05 08:24:55 -0300

    feat: add initial layout and components

---
+++ level-1/challenge-todo-list/src/App.tsx
@@ -0,0 +1,23 @@
+import { PlusCircle } from "@phosphor-icons/react"
+import styles from "./App.module.css"
+import { Button } from "./components/Button/Button.js"
+import { Input } from "./components/Input/Input.js"
+import { Header } from "./components/Header/Header.js"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <section className={styles.content}>
+        <div className={styles.createTaskContainer}>
+          <Input placeholder="Adicione uma nova tarefa" />
+          <Button>
+            Criar <PlusCircle size={16} color="#f2f2f2" weight="bold" />
+          </Button>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
