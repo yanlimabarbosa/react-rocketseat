176b9d557069646bf911a4af0180f834e41fda37
Yan Lima Barbosa <yanbr763@gmail.com>
2024-09-06 06:57:14 -0300

    feat: Introduce global styles
    Refactor global styles to improve consistency and enhance user experience.
    
    - Introduce a global style sheet to define base styles for elements.
    - Apply consistent typography and color scheme.
    - Add focus styles to improve accessibility.
    - Leverage `styled-components` for theme-aware styling.

--- level-2/02-ignite-timer/src/styles/global.ts
+++ level-2/02-ignite-timer/src/styles/global.ts
@@ -1,14 +1,27 @@
 import { createGlobalStyle } from "styled-components";
+import { DefaultTheme } from "./../../@types/styled.d"
 
 export const GlobalStyle = createGlobalStyle`
-  *{
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
 
-  body{
-    background: #333;
+  :focus{
+    outline: 0;
+    box-shadow: 0 0 0 2px ${props => props.theme["green-500"]};
+  }
+
+  body {
+    background: ${props => props.theme["gray-900"]};
+    color: ${props => props.theme["gray-300"]};
     color: #fff;
+  }
+
+  body, input-security ,textarea ,button {
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
   }
 `
