2de5a27f757385c8e76bfb7db6ebb7c75e80546c
Yan Lima Barbosa <yanbr763@gmail.com>
2024-09-14 19:47:32 -0300

    TypeScript on Form

--- level-2/02-ignite-timer/src/pages/Home/index.tsx
+++ level-2/02-ignite-timer/src/pages/Home/index.tsx
@@ -21,19 +21,23 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
 })
 
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
-  const { register, handleSubmit, watch, formState } = useForm({
+  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
   })
 
-  function handleCreateNewCycle(data: any) {
+  function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
   }
 
   const task = watch('task')
   const isSubmitDisabled = !task
-
-  console.log(formState)
 
   return (
     <HomeContainer>
