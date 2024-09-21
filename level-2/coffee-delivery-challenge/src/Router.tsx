8885f2ec8a4913d39c04341c880d8a73da4a808d
Yan Lima Barbosa <yanbr763@gmail.com>
2024-09-21 02:16:18 -0300

    feat: Implement default layout
    Introduces a default layout for the application, providing a consistent header and container structure for all pages. This enhances user experience and simplifies page development.

--- level-2/coffee-delivery-challenge/src/Router.tsx
+++ level-2/coffee-delivery-challenge/src/Router.tsx
@@ -1,10 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
-import { Home } from '../pages/Home'
+import { DefaultLayout } from './layouts/DefaultLayout'
+import { Home } from './pages/Home'
+import { Checkout } from './pages/Checkout'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Route>
     </Routes>
   )
 }
