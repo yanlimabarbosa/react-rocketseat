3efbaade6e4524c4f4427fb56fc3e23687026f81
Yan Lima Barbosa <yanbr763@gmail.com>
2024-09-21 20:00:22 -0300

    feat: add intro section with benefits
    Adds an intro section with a background image, title, description, and benefits section to the Home page. This provides an engaging introduction to the Coffee Delivery platform and highlights its key selling points.

--- level-2/coffee-delivery-challenge/src/pages/Home/components/Intro/styles.ts
+++ level-2/coffee-delivery-challenge/src/pages/Home/components/Intro/styles.ts
@@ -1,8 +1,23 @@
 import styled from 'styled-components'
+import introBackgroundImg from '../../../../assets/intro-background.png'
+import { rgba } from 'polished'
+import { TitleText } from '../../../../components/Typography'
 
 export const IntroContainer = styled.section`
   width: 100%;
   height: 34rem;
+  background: ${({ theme }) => `url(${introBackgroundImg}) no-repeat center,
+      linear-gradient(
+        0deg,
+        ${theme.colors.white} 0%,
+        ${rgba(theme.colors.white, 1)} 100%,
+        ${theme.colors.white} 100%
+      )`};
+  background-size: cover;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 export const IntroContent = styled.div`
@@ -10,4 +25,16 @@ export const IntroContent = styled.div`
   align-items: center;
   justify-content: space-between;
   gap: 3.5rem;
+`
+
+export const IntroTitle = styled(TitleText)`
+  margin-bottom: '1rem';
+`
+
+export const BenefitsContainer = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  row-gap: 1.25rem;
+  margin-top: 4.125rem;
 `
