48c3d4b7922677ae116b6e49cda4575501af5293
Yan Lima Barbosa <yanbr763@gmail.com>
2024-09-22 16:10:48 -0300

    feat: Add navigation to checkout page
    Adds a navigation link to the checkout page from the header. This allows users to easily access their cart and proceed with the checkout process.

--- level-2/coffee-delivery-challenge/src/components/Header/index.tsx
+++ level-2/coffee-delivery-challenge/src/components/Header/index.tsx
@@ -1,20 +1,25 @@
 import { MapPin, ShoppingCart } from 'phosphor-react'
 import logo from '../../assets/coffe-delivery-logo.svg'
 import { HeaderButton, HeaderButtonsContainer, HeaderContainer } from './styles'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <HeaderContainer>
       <div className="container">
-        <img src={logo} />
+        <NavLink to="/">
+          <img src={logo} />
+        </NavLink>
         <HeaderButtonsContainer>
           <HeaderButton variant="purple">
             <MapPin size={20} weight="fill" />
             Porto Alegre, RS
           </HeaderButton>
-          <HeaderButton variant="yellow">
-            <ShoppingCart size={20} weight="fill" />
-          </HeaderButton>
+          <NavLink to="/checkout">
+            <HeaderButton variant="yellow">
+              <ShoppingCart size={20} weight="fill" />
+            </HeaderButton>
+          </NavLink>
         </HeaderButtonsContainer>
       </div>
     </HeaderContainer>
