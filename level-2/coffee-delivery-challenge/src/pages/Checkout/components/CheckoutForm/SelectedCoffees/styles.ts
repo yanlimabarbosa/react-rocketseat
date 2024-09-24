50d0868b4018561d623dc702b9039d5235ecbeda
Yan Lima Barbosa <yanbr763@gmail.com>
2024-09-23 22:39:14 -0300

    feat: implement selected coffees component
    Adds a new component to display selected coffees in the checkout form. This component renders a list of coffee cards for each selected coffee, providing a visual representation of the user's order.

---
+++ level-2/coffee-delivery-challenge/src/pages/Checkout/components/CheckoutForm/SelectedCoffees/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+import { SectionBaseStyle } from '../../../styles'
+
+export const SelectedCoffeesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+  max-width: 40rem;
+  flex: 1;
+`
+
+export const DetailsContainer = styled(SectionBaseStyle)`
+  border-radius: 6px 44px 6px 44px;
+  display: flex;
+  flex-direction: column;
+`
