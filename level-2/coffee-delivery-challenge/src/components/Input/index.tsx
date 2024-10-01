f9c98d39260370787153daa2c3bc1a4b229e9ceb
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-01 04:04:12 -0300

    feat: add error and right text support to input component
    Adds error and right text functionality to the input component, allowing for clear error messages and additional context alongside the input field.

--- level-2/coffee-delivery-challenge/src/components/Input/index.tsx
+++ level-2/coffee-delivery-challenge/src/components/Input/index.tsx
@@ -1,8 +1,32 @@
-import { InputHTMLAttributes } from 'react'
-import { InputStyleContainer } from './styles'
+import { forwardRef, InputHTMLAttributes } from "react"
+import {
+  InputStyleContainer,
+  InputStyled,
+  InputWrapper,
+  RightText,
+} from "./styles"
+import { RegularText } from "../Typography"
 
-type InputProps = InputHTMLAttributes<HTMLInputElement>
-
-export function Input({ ...props }: InputProps) {
-  return <InputStyleContainer {...props} />
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+  error?: string
+  rightText?: string
 }
+
+// old:
+// export function Input({ ...props }: InputProps) {
+//   return <InputStyleContainer {...props} />
+// }
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ error, className, rightText, ...props }, ref) => {
+    return (
+      <InputWrapper className={className}>
+        <InputStyleContainer>
+          <InputStyled {...props} ref={ref} />
+          {rightText && <RightText>{rightText}</RightText>}
+        </InputStyleContainer>
+        {error && <RegularText size="s">{error}</RegularText>}
+      </InputWrapper>
+    )
+  }
+)
--- level-2/coffee-delivery-challenge/src/components/Input/styles.ts
+++ level-2/coffee-delivery-challenge/src/components/Input/styles.ts
@@ -1,20 +1,55 @@
 import styled from 'styled-components'
 
-export const InputStyleContainer = styled.input`
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  position: relative;
+  transition: 0.4s;
+
+  > p {
+    color: ${({ theme }) => theme.colors['base-error']}
+  }
+`
+
+export const InputStyleContainer = styled.div`
   height: 2.625rem;
   border-radius: 4px;
   border: 1px solid ${({ theme }) => theme.colors['base-button']};
   background: ${({ theme }) => theme.colors['base-input']};
-  transition: 0.4s;
 
-  &:focus {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  transition: 0.4s;
+  overflow: hidden;
+
+  /* if the input within it focus, this div will active the border */
+  &:focus-within {
     border-color: ${({ theme }) => theme.colors['yellow-dark']};
   }
+`
 
-  color: ${({ theme }) => theme.colors['base-text']};
+export const InputStyled = styled.input`
+  flex: 1;
+  height: 100%;
+
+  background: none;
+  border: none;
+
   font-size: 0.75rem;
   padding: 0.75rem;
+  color: ${({ theme }) => theme.colors['base-text']};
+
   &::placeholder {
     color: ${({ theme }) => theme.colors['base-label']};
-  }
+  } 
+`
+
+export const RightText = styled.p`
+  font-size: 0.75rem;
+  margin-right: 0.75rem;
+  font-style: italic;
+  color: ${({ theme }) => theme.colors['base-label']};
 `
