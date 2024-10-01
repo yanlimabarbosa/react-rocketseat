bd49c6d9c01aa5d0a5540999652149ad98eb0abf
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-01 04:07:34 -0300

    feat: Add error state for Input component
    Add a visual error state to the Input component. When an error is present, the input border turns red and a corresponding message is displayed. This improves the user experience by providing clear feedback on input errors.

--- level-2/coffee-delivery-challenge/src/components/Input/styles.ts
+++ level-2/coffee-delivery-challenge/src/components/Input/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css } from "styled-components"
 
 export const InputWrapper = styled.div`
   display: flex;
@@ -8,15 +8,19 @@ export const InputWrapper = styled.div`
   transition: 0.4s;
 
   > p {
-    color: ${({ theme }) => theme.colors['base-error']}
+    color: ${({ theme }) => theme.colors["base-error"]};
   }
 `
 
-export const InputStyleContainer = styled.div`
+interface InputStyleContainerProps {
+  hasError: boolean
+}
+
+export const InputStyleContainer = styled.div<InputStyleContainerProps>`
   height: 2.625rem;
   border-radius: 4px;
-  border: 1px solid ${({ theme }) => theme.colors['base-button']};
-  background: ${({ theme }) => theme.colors['base-input']};
+  border: 1px solid ${({ theme }) => theme.colors["base-button"]};
+  background: ${({ theme }) => theme.colors["base-input"]};
 
   display: flex;
   justify-content: space-between;
@@ -27,8 +31,14 @@ export const InputStyleContainer = styled.div`
 
   /* if the input within it focus, this div will active the border */
   &:focus-within {
-    border-color: ${({ theme }) => theme.colors['yellow-dark']};
+    border-color: ${({ theme }) => theme.colors["yellow-dark"]};
   }
+
+  ${({ theme, hasError }) =>
+    hasError &&
+    css`
+      border-color: ${theme.colors["base-error"]};
+    `}
 `
 
 export const InputStyled = styled.input`
@@ -40,16 +50,16 @@ export const InputStyled = styled.input`
 
   font-size: 0.75rem;
   padding: 0.75rem;
-  color: ${({ theme }) => theme.colors['base-text']};
+  color: ${({ theme }) => theme.colors["base-text"]};
 
   &::placeholder {
-    color: ${({ theme }) => theme.colors['base-label']};
-  } 
+    color: ${({ theme }) => theme.colors["base-label"]};
+  }
 `
 
 export const RightText = styled.p`
   font-size: 0.75rem;
   margin-right: 0.75rem;
   font-style: italic;
-  color: ${({ theme }) => theme.colors['base-label']};
+  color: ${({ theme }) => theme.colors["base-label"]};
 `
