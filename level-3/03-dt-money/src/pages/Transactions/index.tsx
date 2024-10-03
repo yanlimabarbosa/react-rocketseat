8027b00fae960b57521efeaecfc59731e5714cd4
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-03 02:14:46 -0300

    feat: fetch and display transactions
    This commit introduces the ability to fetch transactions from a mock server and display them in a table. This feature enhances the user experience by providing a visual representation of transactions and their associated details.

--- level-3/03-dt-money/src/pages/Transactions/index.tsx
+++ level-3/03-dt-money/src/pages/Transactions/index.tsx
@@ -2,8 +2,31 @@ import { Header } from "../../components/Header";
 import { Sumary } from "../../components/Sumary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+import { useEffect, useState } from "react";
+
+interface Transaction {
+  id: number
+  description: string
+  type: "income" | "outcome"
+  price: "number"
+  category: "string"
+  createdAt: "string"
+}
 
 export function Transactions() {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  async function loadTransactions() {
+    const response = await fetch("http://localhost:3000/transactions")
+    const data = await response.json()
+
+    setTransactions(data)
+  }
+
+  useEffect(() => {
+    loadTransactions()
+  }, [])
+
   return (
     <div>
       <Header />
@@ -11,22 +34,20 @@ export function Transactions() {
       <TransactionsContainer>
         <SearchForm />
         <TransactionsTable>
-          <tr>
-            <td width="50%">Desenvolvimento de site</td>
-            <td>
-              <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
-            </td>
-            <td>Venda</td>
-            <td>13/04/2022</td>
-          </tr>
-          <tr>
-            <td width="50%">Hamburguer</td>
-            <td>
-              <PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
-            </td>
-            <td>Alimentação</td>
-            <td>13/04/2022</td>
-          </tr>
+          <tbody>
+            {transactions.map(transaction => {
+              return (
+                <tr>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighlight variant="income">{transaction?.price}</PriceHighlight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.createdAt}</td>
+                </tr>
+              )
+            })}
+          </tbody>
         </TransactionsTable>
       </TransactionsContainer>
     </div>
