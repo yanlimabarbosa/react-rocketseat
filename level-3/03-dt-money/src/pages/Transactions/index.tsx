ade59b63e144e2c30db13531de14007aebfa7cc3
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-02 22:54:48 -0300

    feat: Implement transactions table
    Adds a table to display transaction information, including date, description, amount, and type. Styles are added to enhance the visual presentation and differentiate between income and outcome transactions.

--- level-3/03-dt-money/src/pages/Transactions/index.tsx
+++ level-3/03-dt-money/src/pages/Transactions/index.tsx
@@ -1,11 +1,33 @@
 import { Header } from "../../components/Header";
 import { Sumary } from "../../components/Sumary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Sumary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tr>
+            <td width="50%">Desenvolvimento de site</td>
+            <td>
+              <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+            </td>
+            <td>Venda</td>
+            <td>13/04/2022</td>
+          </tr>
+          <tr>
+            <td width="50%">Hamburguer</td>
+            <td>
+              <PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
+            </td>
+            <td>Alimentação</td>
+            <td>13/04/2022</td>
+          </tr>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
