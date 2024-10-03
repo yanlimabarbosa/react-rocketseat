7ab1626700664f167ce8e005b8527225316304d6
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-03 02:25:28 -0300

    feat: Implement transaction context
    Introduce a context to manage transactions throughout the application, allowing data sharing between components. This change enables features like:
    
    - Centralized transaction storage
    -  Sumary component updates based on transaction data
    
    This paves the way for enhanced functionality and a more robust transaction management system.

--- level-3/03-dt-money/src/pages/Transactions/index.tsx
+++ level-3/03-dt-money/src/pages/Transactions/index.tsx
@@ -1,31 +1,12 @@
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Sumary } from "../../components/Sumary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
-import { useEffect, useState } from "react";
-
-interface Transaction {
-  id: number
-  description: string
-  type: "income" | "outcome"
-  price: "number"
-  category: "string"
-  createdAt: "string"
-}
+import { TransactionContext } from "../../../contexts/TransactionsContext";
 
 export function Transactions() {
-  const [transactions, setTransactions] = useState<Transaction[]>([])
-
-  async function loadTransactions() {
-    const response = await fetch("http://localhost:3000/transactions")
-    const data = await response.json()
-
-    setTransactions(data)
-  }
-
-  useEffect(() => {
-    loadTransactions()
-  }, [])
+  const { transactions } = useContext(TransactionContext)
 
   return (
     <div>
