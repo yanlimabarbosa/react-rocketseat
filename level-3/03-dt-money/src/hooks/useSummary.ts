dd96a765dbb3671de655b09f4580b8c2fe619438
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-04 03:14:37 -0300

    feat: Memoize summary calculation
    Memoize the summary calculation to improve performance by preventing redundant calculations on every render.

--- level-3/03-dt-money/src/hooks/useSummary.ts
+++ level-3/03-dt-money/src/hooks/useSummary.ts
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import { TransactionContext } from '../../contexts/TransactionsContext'
 import { useContextSelector } from 'use-context-selector'
 
@@ -6,20 +7,22 @@ export function useSummary() {
     return context.transactions
   })
 
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      }
-      if (transaction.type === 'outcome') {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
-      return acc
-    },
-    { income: 0, outcome: 0, total: 0 },
-  )
+  const summary = useMemo(() => {
+    transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.price
+          acc.total += transaction.price
+        }
+        if (transaction.type === 'outcome') {
+          acc.outcome += transaction.price
+          acc.total -= transaction.price
+        }
+        return acc
+      },
+      { income: 0, outcome: 0, total: 0 },
+    )
+  }, [transactions])
 
   return summary
 }
